f8fe0c5b6e65ac5d8f6051558e335f3f3a20d94b
G-Just <gjustas00@gmail.com>
2024-03-29 12:59:29 +0200

    Adding seeding for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'IS1ca7122a5fe82d',
             'email' => '98f87fd651@gmail.com',
             'password' => Hash::make('123')
         ]);
+
+        for ($i = 0; $i < 50; $i++) {
+            DB::table('users')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10) . '@example.com',
+                'password' => Hash::make('password'),
+            ]);
+        }
+
+        for ($i = 2; $i < 20; $i++) {
+            DB::table('ratings')->insert([
+                'rating' => 10,
+                'comment' => Str::random(50),
+                'user_id' => $i,
+                'movie_id' => 1
+            ]);
+        }
     }
 }
