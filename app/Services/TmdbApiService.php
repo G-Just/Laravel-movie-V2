65461ca5a7cea860a335b1c649da685d650c26e0
G-Just <gjustas00@gmail.com>
2024-03-29 04:25:51 +0200

    Refactoring and reducing duplicate code

--- app/Services/TmdbApiService.php
+++ app/Services/TmdbApiService.php
@@ -94,21 +94,15 @@ class TmdbApiService
                 return Cache::get('popularShows');
             } else {;
                 $content = Http::get('https://api.themoviedb.org/3/trending/tv/week?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['title'] = $content[$key]['name'];
-                    $content[$key]['release_date'] = $content[$key]['first_air_date'];
-                    $content[$key]['type'] = 'Show';
-                    Cache::put('popularShows', $content, now()->addDay());
-                }
+                $this->unifyShows($content);
+                Cache::put('popularShows', $content, now()->addDay());
             }
         } else {
             if (Cache::has('popularMovies')) {
                 return Cache::get('popularMovies');
             } else {
                 $content = Http::get('https://api.themoviedb.org/3/movie/popular?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['type'] = 'Movie';
-                };
+                $this->unifyMovies($content);
                 Cache::put('popularMovies', $content, now()->addDay());
             }
         }
@@ -122,21 +116,15 @@ class TmdbApiService
                 return Cache::get('currentShows');
             } else {;
                 $content = Http::get('https://api.themoviedb.org/3/tv/airing_today?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['title'] = $content[$key]['name'];
-                    $content[$key]['release_date'] = $content[$key]['first_air_date'];
-                    $content[$key]['type'] = 'Show';
-                    Cache::put('currentShows', $content, now()->addDay());
-                }
+                $this->unifyShows($content);
+                Cache::put('currentShows', $content, now()->addDay());
             }
         } else {
             if (Cache::has('currentMovies')) {
                 return Cache::get('currentMovies');
             } else {
                 $content = Http::get('https://api.themoviedb.org/3/movie/now_playing?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['type'] = 'Movie';
-                };
+                $this->unifyMovies($content);
                 Cache::put('currentMovies', $content, now()->addDay());
             }
         }
@@ -150,21 +138,15 @@ class TmdbApiService
                 return Cache::get('topShows');
             } else {;
                 $content = Http::get('https://api.themoviedb.org/3/tv/top_rated?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['title'] = $content[$key]['name'];
-                    $content[$key]['release_date'] = $content[$key]['first_air_date'];
-                    $content[$key]['type'] = 'Show';
-                    Cache::put('topShows', $content, now()->addDay());
-                }
+                $this->unifyShows($content);
+                Cache::put('topShows', $content, now()->addDay());
             }
         } else {
             if (Cache::has('topMovies')) {
                 return Cache::get('topMovies');
             } else {
                 $content = Http::get('https://api.themoviedb.org/3/movie/top_rated?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['type'] = 'Movie';
-                };
+                $this->unifyMovies($content);
                 Cache::put('topMovies', $content, now()->addDay());
             }
         }
@@ -178,24 +160,38 @@ class TmdbApiService
                 return Cache::get('upcomingShows');
             } else {;
                 $content = Http::get('https://api.themoviedb.org/3/tv/on_the_air?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['title'] = $content[$key]['name'];
-                    $content[$key]['release_date'] = $content[$key]['first_air_date'];
-                    $content[$key]['type'] = 'Show';
-                    Cache::put('upcomingShows', $content, now()->addDay());
-                }
+                $this->unifyShows($content);
+                Cache::put('upcomingShows', $content, now()->addDay());
             }
         } else {
             if (Cache::has('upcomingMovies')) {
                 return Cache::get('upcomingMovies');
             } else {
                 $content = Http::get('https://api.themoviedb.org/3/movie/upcoming?' . $this->tmdbKey)->collect('results')->toArray();
-                foreach ($content as $key => $movie) {
-                    $content[$key]['type'] = 'Movie';
-                };
+                $this->unifyMovies($content);
                 Cache::put('upcomingMovies', $content, now()->addDay());
             }
         }
+        return $content;
+    }
+
+    private function unifyMovies($content)
+    {
+        foreach ($content as $key => $movie) {
+            $content[$key]['type'] = 'Movie';
+        };
+
+        return $content;
+    }
+
+    private function unifyShows($content)
+    {
+        foreach ($content as $key => $show) {
+            $content[$key]['title'] = $content[$key]['name'];
+            $content[$key]['release_date'] = $content[$key]['first_air_date'];
+            $content[$key]['type'] = 'Show';
+        }
+
         return $content;
     }
 
